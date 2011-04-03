5ea12ffe7033d9841f822051527e0068bcb889be
lepnio <lepnio@gmail.com>
2011-04-03 21:50:30 +0200

    Add two examples

---
+++ chapter-1/src/scjp/chapter/one/peiniau/QuestionOne.java
@@ -0,0 +1,12 @@
+package scjp.chapter.one.peiniau;
+
+public class QuestionOne {
+	
+	public void example() {
+		System.out.println("This is an example");
+	}
+
+	public static void main(String... args) {
+		new QuestionOne().example();
+	}
+}
